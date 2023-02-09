c4216e742825104968732460633a4563481d7b77
Kraimps Pierre <pier_lupo@laposte.net>
2023-02-09 08:41:41 +0100

    demo-react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import ThirdComponent, { FirstComponent, SecondComponent, FourthComponent, FifthComponent, SixthComponent } from './Component/Component';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Hello</h1>  
+      <h2>Christophe</h2> 
+      <FirstComponent></FirstComponent> 
+      <FirstComponent /> 
+      <SecondComponent />
+      <ThirdComponent test = "test" monprops = "component statefull"></ThirdComponent>
+      <FourthComponent message ={"ceci est un props"}></FourthComponent>
+      <FifthComponent message ={"ceci est un mon cinquième composant"}></FifthComponent>
+      <SixthComponent>
+        <div>Et enfin mon sixième composant</div>
+      </SixthComponent>
       </header>
     </div>
   );
---
+++ src/Component/Component.js
@@ -0,0 +1,64 @@
+import { Component } from "react";
+
+//la bonne pratique est de nommer les fonctions en PascalCase:
+export function FirstComponent(){
+    return (
+        <header>
+            <h3>Mon premier component React</h3>
+        </header>
+    )
+} 
+
+export const SecondComponent = () => {
+    return (
+    <div>
+    <h4> Mon second component React</h4>
+    </div>
+    )
+}
+
+class ThirdComponent extends Component {
+    constructor(props){
+        super(props)
+        this.state = {message : props.monprops}
+    }
+
+    render(){
+        return (
+            <div>
+            <h5> Mon troisième component React</h5>
+            <h5> Message: {this.state.message}</h5>
+            </div>
+        )
+    }
+}
+
+export default ThirdComponent
+
+export function FourthComponent (props) {
+   
+        return (
+            <div>
+            <h5>{props.message}</h5>
+            </div>
+        )
+    
+}
+
+export function FifthComponent({message}){
+    return (
+        <div>
+            <h1>{message}</h1>
+        </div>
+    )
+}
+
+
+export function SixthComponent(props){
+    console.log(props)
+    return (
+        <div>
+            <h1>{props.children}</h1>
+        </div>
+    )
+}
